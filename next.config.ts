c58190ca0a23792c984823025bcf982906833429
Vivek Tarnallya <116621113+vivekbeast@users.noreply.github.com>
2024-12-25 15:46:03 +0530

    second commit

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['lh3.googleusercontent.com']
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 };
 
 export default nextConfig;
