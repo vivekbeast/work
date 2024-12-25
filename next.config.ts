1981c49ec4ab61bb8b087fd24fc8c718bab6dfc1
Vivek Tarnallya <116621113+vivekbeast@users.noreply.github.com>
2024-12-25 16:49:26 +0530

    SIXTH commit

--- next.config.ts
+++ next.config.ts
@@ -12,17 +12,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-// next.config.js
-
-module.exports = {
-  eslint: {
-    // Disable specific ESLint rules
-    ignoreDuringBuilds: true, // Ignore ESLint during build, you can modify this if you prefer strict linting.
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn', // Show a warning for unused variables
-      '@typescript-eslint/explicit-function-return-type': 'off', // Disable explicit return type rule
-      '@typescript-eslint/explicit-module-boundary-types': 'off', // Disable explicit return type for module boundaries
-    },
-  },
-};
