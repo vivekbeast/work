c01d88a62d06e1c9da24662e2544c69d9dfaa1f2
Vivek Tarnallya <116621113+vivekbeast@users.noreply.github.com>
2024-12-25 16:09:36 +0530

    FOURTH commit

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -7,8 +7,8 @@ import Google from "next-auth/providers/google";
 const authOption = {
     providers: [
         Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
         }),
     ],
     callbacks: {
